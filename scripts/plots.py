713e3aae7d7600a895f93090ad38f2b37b6f5c10
Maelaf Estiphanos <85798214+Maelaf@users.noreply.github.com>
2022-04-06 21:00:54 +0300

    Update plots.py

--- scripts/plots.py
+++ scripts/plots.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
-
+'''histogram plot function'''
 def plot_hist(df: pd.DataFrame, column: str, color: str) -> None:
     plt.figure(figsize=(9, 7))
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
